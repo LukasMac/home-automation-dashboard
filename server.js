51315f442e1f3ebeff08151ed217f86d21ab118c
Lukas Maciulis <lukasm@spotify.com>
2018-11-10 00:58:55 +0100

    Express on port 8081 and start it in docker

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express')
-const app = express()
-const port = 3001
+const express = require('express');
+const app = express();
+const port = 8081;
 
 app.use(express.static('dist'));
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
