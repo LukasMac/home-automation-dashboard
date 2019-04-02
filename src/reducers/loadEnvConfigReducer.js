4f31345e8128516b512ab66563844a2463ce8bb1
Lukas Maciulis <lukasm@spotify.com>
2019-04-02 21:40:42 +0200

    Prettify

--- src/reducers/loadEnvConfigReducer.js
+++ src/reducers/loadEnvConfigReducer.js
@@ -1,21 +1,21 @@
-import { types } from '../actions/loadEnvConfigActions';
+import { types } from "../actions/loadEnvConfigActions";
 
-const initialState = 'not loaded yet';
+const initialState = "not loaded yet";
 
 export default (state = initialState, action) => {
-  switch(action.type) {
+  switch (action.type) {
     case types.LOAD_ENV_CONFIG_REQUEST:
-      return 'loading....';
+      return "loading....";
     case types.LOAD_ENV_CONFIG_SUCCESS:
       const data = action.data.data;
       Object.keys(data).forEach(key => {
         localStorage.setItem(key, data[key]);
       });
 
-      return 'Success';
+      return "Success";
     case types.LOAD_ENV_CONFIG_FAILURE:
       return JSON.stringify(action.error);
     default:
       return state;
   }
-}
+};
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,27 +1,27 @@
 // For info about this file refer to webpack and webpack-hot-middleware documentation
 // For info on how we're generating bundles with hashed filenames for cache busting: https://medium.com/@okonetchnikov/long-term-caching-of-static-assets-with-webpack-1ecb139adb95#.w99i89nsz
-import webpack from 'webpack';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-import path from 'path';
+import webpack from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import path from "path";
 
 const GLOBALS = {
-  'process.env.NODE_ENV': JSON.stringify('production'),
+  "process.env.NODE_ENV": JSON.stringify("production"),
   __DEV__: false
 };
 
 export default {
   resolve: {
-    extensions: ['*', '.js', '.jsx', '.json']
+    extensions: ["*", ".js", ".jsx", ".json"]
   },
-  devtool: 'source-map', // more info:https://webpack.js.org/guides/production/#source-mapping and https://webpack.js.org/configuration/devtool/
-  entry: path.resolve(__dirname, 'src/index'),
-  target: 'web',
-  mode: 'production',
+  devtool: "source-map", // more info:https://webpack.js.org/guides/production/#source-mapping and https://webpack.js.org/configuration/devtool/
+  entry: ["@babel/polyfill", path.resolve(__dirname, "src/index")],
+  target: "web",
+  mode: "production",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
-    filename: '[name].[contenthash].js'
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+    filename: "[name].[contenthash].js"
   },
   plugins: [
     // Tells React to build in prod mode. https://facebook.github.io/react/downloads.html
@@ -29,13 +29,13 @@ export default {
 
     // Generate an external css file with a hash in the filename
     new MiniCssExtractPlugin({
-      filename: '[name].[contenthash].css'
+      filename: "[name].[contenthash].css"
     }),
 
     // Generate HTML file that contains references to generated bundles. See here for how this works: https://github.com/ampedandwired/html-webpack-plugin#basic-usage
     new HtmlWebpackPlugin({
-      template: 'src/index.ejs',
-      favicon: 'src/favicon.ico',
+      template: "src/index.ejs",
+      favicon: "src/favicon.ico",
       minify: {
         removeComments: true,
         collapseWhitespace: true,
@@ -51,24 +51,23 @@ export default {
       inject: true,
       // Note that you can add custom options here if you need to handle other custom logic in index.html
       // To track JavaScript errors via TrackJS, sign up for a free trial at TrackJS.com and enter your token below.
-      trackJSToken: ''
-    }),
-
+      trackJSToken: ""
+    })
   ],
   module: {
     rules: [
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        use: ['babel-loader']
+        use: ["babel-loader"]
       },
       {
         test: /\.eot(\?v=\d+.\d+.\d+)?$/,
         use: [
           {
-            loader: 'url-loader',
+            loader: "url-loader",
             options: {
-              name: '[name].[ext]'
+              name: "[name].[ext]"
             }
           }
         ]
@@ -77,11 +76,11 @@ export default {
         test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
         use: [
           {
-            loader: 'url-loader',
+            loader: "url-loader",
             options: {
               limit: 10000,
-              mimetype: 'application/font-woff',
-              name: '[name].[ext]'
+              mimetype: "application/font-woff",
+              name: "[name].[ext]"
             }
           }
         ]
@@ -90,11 +89,11 @@ export default {
         test: /\.[ot]tf(\?v=\d+.\d+.\d+)?$/,
         use: [
           {
-            loader: 'url-loader',
+            loader: "url-loader",
             options: {
               limit: 10000,
-              mimetype: 'application/octet-stream',
-              name: '[name].[ext]'
+              mimetype: "application/octet-stream",
+              name: "[name].[ext]"
             }
           }
         ]
@@ -103,11 +102,11 @@ export default {
         test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
         use: [
           {
-            loader: 'url-loader',
+            loader: "url-loader",
             options: {
               limit: 10000,
-              mimetype: 'image/svg+xml',
-              name: '[name].[ext]'
+              mimetype: "image/svg+xml",
+              name: "[name].[ext]"
             }
           }
         ]
@@ -116,9 +115,9 @@ export default {
         test: /\.(jpe?g|png|gif|ico)$/i,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
             options: {
-              name: '[name].[ext]'
+              name: "[name].[ext]"
             }
           }
         ]
@@ -128,23 +127,22 @@ export default {
         use: [
           MiniCssExtractPlugin.loader,
           {
-            loader: 'css-loader',
+            loader: "css-loader",
             options: {
               sourceMap: true
             }
-          }, {
-            loader: 'postcss-loader',
+          },
+          {
+            loader: "postcss-loader",
             options: {
-              plugins: () => [
-                require('cssnano'),
-                require('autoprefixer'),
-              ],
+              plugins: () => [require("cssnano"), require("autoprefixer")],
               sourceMap: true
             }
-          }, {
-            loader: 'sass-loader',
+          },
+          {
+            loader: "sass-loader",
             options: {
-              includePaths: [path.resolve(__dirname, 'src', 'scss')],
+              includePaths: [path.resolve(__dirname, "src", "scss")],
               sourceMap: true
             }
           }
