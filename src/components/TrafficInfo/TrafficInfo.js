e6dfed69c5b76b50015f836a14041db21c0e61e4
Lukas Maciulis <lukasm@spotify.com>
2019-11-30 22:31:46 +0100

    Make traffic dashboard more like SL

--- src/components/TrafficInfo/TrafficInfo.js
+++ src/components/TrafficInfo/TrafficInfo.js
@@ -1,75 +1,118 @@
-import './TrafficInfo.scss';
-import PropTypes from 'prop-types';
-import React from 'react';
-import Moment from 'moment';
+import "./TrafficInfo.scss";
+import PropTypes from "prop-types";
+import React from "react";
+import Moment from "moment";
 
 export default class TrafficInfo extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { currentTime: '' };
+    this.state = { currentTime: "" };
     this.props.onRefresh();
     setInterval(this.ticker.bind(this), 1000);
   }
 
   ticker() {
-    this.setState({ currentTime: Moment().format('HH:mm:ss') });
+    const shouldRefresh = Moment().second() % 30 === 0;
+    if (shouldRefresh) {
+      this.props.onRefresh();
+    }
+
+    this.setState({ currentTime: Moment().format("HH:mm:ss") });
   }
 
   buses() {
     let { Buses: buses = [] } = this.props;
 
-    buses = buses.filter(bus => bus.JourneyDirection === 2)
+    buses = buses.filter(bus => bus.JourneyDirection === 2);
 
     if (buses.length === 0) {
-      return <div>No Busses in upcoming 20mins</div>;
+      return <div className="elipsis">No Busses in next 20mins</div>;
     }
 
-    return buses
-      .map( bus => {
-        return (
-          <div key={bus.JourneyNumber}>{bus.TransportMode} {bus.LineNumber} to {bus.Destination} {Moment(bus.ExpectedDateTime).fromNow()} ({Moment(bus.ExpectedDateTime).format('HH:mm:ss')})</div>
-          );
-      });
+    const nextTwoDeparturesInMinutes = buses
+      .sort((a, b) => {
+        Moment(a.ExpectedDateTime).unix() > Moment(b.ExpectedDateTime).unix();
+      })
+      .slice(0, 2)
+      .map(buss => {
+        const diffInMinutes = Moment(buss.ExpectedDateTime).diff(
+          Moment(),
+          "minutes"
+        );
+        if (diffInMinutes <= 0) {
+          return "Nu";
+        }
+
+        return diffInMinutes;
+      })
+      .join(",\u00A0");
+
+    const buss = buses[0];
+    return (
+      <div className="flex">
+        <div className="direction elipsis">
+          {buss.LineNumber} {buss.Destination}
+        </div>
+        <div className="nextDeparture elipsis">
+          {nextTwoDeparturesInMinutes}
+        </div>
+      </div>
+    );
   }
 
   trams() {
-    let{ Trams: trams = [] } = this.props;
+    let { Trams: trams = [] } = this.props;
 
-    trams = trams.filter(tram =>tram.JourneyDirection === 1)
+    trams = trams.filter(tram => tram.JourneyDirection === 1);
 
     if (trams.length === 0) {
-      return <div>No Trams in upcoming 20mins</div>;
+      return <div className="elipsis">No Trams in next 20mins</div>;
     }
 
-    return trams
-      // .filter(bus => bus.JourneyDirection === 1)
-      .map( tram => {
-        return (
-          <div key={tram.JourneyNumber}>{tram.TransportMode} to {tram.Destination} {Moment(tram.ExpectedDateTime).fromNow()} ({Moment(tram.ExpectedDateTime).format('HH:mm:ss')})</div>
-          );
-      });
+    const nextTwoDeparturesInMinutes = trams
+      .sort((a, b) => {
+        Moment(a.ExpectedDateTime).unix() > Moment(b.ExpectedDateTime).unix();
+      })
+      .filter(tram => tram.JourneyDirection === 1)
+      .slice(0, 2)
+      .map(tram => {
+        const diffInMinutes = Moment(tram.ExpectedDateTime).diff(
+          Moment(),
+          "minutes"
+        );
+        if (diffInMinutes <= 0) {
+          return "Nu";
+        }
+
+        return diffInMinutes;
+      })
+      .join(",\u00A0");
+
+    const tram = trams[0];
+    return (
+      <div className="flex">
+        <div className="direction elipsis">
+          {tram.LineNumber} {tram.Destination}
+        </div>
+        <div className="nextDeparture elipsis">
+          {nextTwoDeparturesInMinutes}
+        </div>
+      </div>
+    );
   }
 
   render() {
-    const { onRefresh } = this.props;
-
-
     return (
       <div className="wrapper">
-        <h2>Traffic from Luma</h2>
-        <h3>Current time: {this.state.currentTime}</h3>
-        <h3>Buses:</h3>
-        {this.buses()}
-        <br />
-        <h3>Trams:</h3>
-        {this.trams()}
-        <button onClick={onRefresh}>Refresh (last update: {Moment(this.props.LatestUpdate).fromNow()})</button>
+        <div className="time">{this.state.currentTime}</div>
+        <div>{this.trams()}</div>
+        <div>{this.buses()}</div>
       </div>
-      );
+    );
   }
 }
 
 TrafficInfo.propTypes = {
-  onRefresh: PropTypes.func.isRequired,
+  onRefresh: PropTypes.func.isRequired
 };
